c7f6f9a330a62b29ab0231e6f12693dfdf9c971a
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2024-11-08 23:07:25 -0300

    Criada a lógica de auto incremento dos ids. Criada a função que cria novos produtos e salva eles no arquivo. Criada a função que exibe os produtos salvos no arquivo.

--- index.cpp
+++ index.cpp
@@ -8,11 +8,11 @@
 #include<time.h> // Para lidar com data e hora
 
 #define NOME_PROJETO "Gerente de produção"
+#define MAX_PRODUTOS 1000
 
 typedef struct { // Definição de produto
 	int id, estoque;
 	char nome[40];
-	bool is_kit;
 } PRODUTO;
 
 typedef struct { // Definição de pedido
@@ -20,10 +20,6 @@ typedef struct { // Definição de pedido
 	char status[20], nome_cliente[50];
 	struct tm data_entrega;
 } PEDIDO;
-
-typedef struct { // Definição dos produtos que compõem um produto_kit
-	int id, produto_id, kit_id, quantidade;
-} PRODUTO_KIT;
 
 typedef struct { // Definição dos produtos que compõem um pedido
 	int id, produto_id, pedido_id, quantidade;
@@ -38,15 +34,29 @@ typedef struct { // Definição dos produtos que compõem uma ordem de procução
 	int id, produto_id, op_id, quantidade;
 } PRODUTO_ORDEM_PRODUCAO;
 
-// Declaração dos contadores de id 
-int produto_count = 1;
-int pedido_count = 1;
-int produto_kit_count = 1;
-int produto_pedido_count = 1;
-int ordem_producao_count = 1;
-int produto_ordem_producao_count = 1;
+typedef struct { // Definição da struct que lida com os auto increments
+	int produto_count;
+	int pedido_count;
+	int produto_pedido_count;
+	int ordem_producao_count;
+	int produto_ordem_producao_count;
+} INCREMENT;
 
 // Prototipação
+
+int menu_inicial();
+int menu_produtos();
+int menu_estoque();
+int menu_pedidos();
+int menu_op();
+void opcao_invalida();
+void valor_invalido();
+void erro_arquivo();
+int get_increment(char counter[30]);
+void novo_produto();
+void save_produto(PRODUTO produto);
+
+// Funções
 
 int menu_inicial() {
 	int n;
@@ -137,19 +147,148 @@ void valor_invalido(char mensagem[40]) {
 	}
 }
 
+void erro_arquivo() {
+	printf("\n\nDeu erro abrindo o arquivo, tenta de novo aí\n\n");
+	system("pause");
+}
+
+int get_increment(char counter[30]) { // Essa função recebe o tipo de dado que será incrementado como string e retorna o último valor de incremento válido.
+	INCREMENT incremento;
+	
+	FILE *arquivo = fopen("auto_increment.dat", "rb");
+	
+	if(arquivo) {
+		fread(&incremento, sizeof(INCREMENT), 1, arquivo);
+		fclose(arquivo);
+	} else {
+		incremento.ordem_producao_count = 1;
+		incremento.pedido_count = 1;
+		incremento.produto_count = 1;
+		incremento.produto_ordem_producao_count = 1;
+		incremento.produto_pedido_count = 1;
+	}
+	
+	if(strcmp(counter, "produto") == 0)
+		return incremento.produto_count;
+	else if(strcmp(counter, "pedido") == 0)
+		return incremento.pedido_count;
+	else if(strcmp(counter, "produto_pedido") == 0)
+		return incremento.produto_pedido_count;
+	else if(strcmp(counter, "ordem_producao") == 0)
+		return incremento.ordem_producao_count;
+	else if(strcmp(counter, "produto_ordem_producao") == 0)
+		return incremento.produto_ordem_producao_count;
+	else
+		printf("\n\n\nTipo de incremento inválido!\\n\n\n");
+}
+
+void save_increment(char counter[30], int id){ // Essa função recebe o tipo de dado que será incrementado e o valor do incremento a ser salvo
+	INCREMENT incremento;
+	
+	FILE *arquivo = fopen("auto_increment.dat",  "rb+");
+	
+	if(arquivo) {
+		fread(&incremento, sizeof(INCREMENT), 1, arquivo);
+		fseek(arquivo, 0, SEEK_SET);
+	} else {
+		arquivo = fopen("auto_increment.dat", "wb");
+		
+		incremento.ordem_producao_count = 1;
+		incremento.pedido_count = 1;
+		incremento.produto_count = 1;
+		incremento.produto_ordem_producao_count = 1;
+		incremento.produto_pedido_count = 1;
+	}
+	
+	if(strcmp(counter, "produto") == 0)
+		incremento.produto_count = id;
+	else if(strcmp(counter, "pedido") == 0)
+		incremento.pedido_count = id;
+	else if(strcmp(counter, "produto_pedido") == 0)
+		incremento.produto_pedido_count = id;
+	else if(strcmp(counter, "ordem_producao") == 0)
+		incremento.ordem_producao_count = id;
+	else if(strcmp(counter, "produto_ordem_producao") == 0)
+		incremento.produto_ordem_producao_count = id;
+	else
+		printf("\n\n\nTipo de incremento inválido!\\n\n\n");
+	
+	fwrite(&incremento, sizeof(INCREMENT), 1, arquivo);
+	fclose(arquivo);
+}
+
+void novo_produto() { // Função que pede para o usuário as informações sobre o novo produto
+	PRODUTO novoProduto;
+	
+	system("cls");
+	
+	printf("Informe a descrição do produto: ");
+	fflush(stdin);
+	gets(novoProduto.nome);
+	
+	printf("Quantidade em estoque do produto: ");
+	scanf("%d", &novoProduto.estoque);
+	
+	novoProduto.id = get_increment("produto"); // Pega o último valor do incremento
+	novoProduto.id++; // Incrementa o incremento
+	
+	save_produto(novoProduto); // Envia o produto para ser salvo
+}
+
+void save_produto(PRODUTO produto) { // Função que salva um produto no arquivo
+	FILE *arquivo = fopen("produtos.dat", "ab"); // Abre o arquivo de produtos para append binário
+	
+	if(!arquivo) { // Retorna uma mensagem para o usuário caso dê problema com o arquivo
+		erro_arquivo();
+		return;
+	}
+	
+	fwrite(&produto, sizeof(PRODUTO), 1, arquivo); // Escreve no arquivo o produto novo
+	fclose(arquivo); // Fecha o arquivo
+	save_increment("produto", produto.id); // Salva o id do último produto como valor do incremento para ser usado no próximo produto cadastrado
+}
+
+void load_produtos(PRODUTO produtos[MAX_PRODUTOS]) { // Função que carrega os produtos do arquivo para o array de PRODUTO
+	FILE *arquivo = fopen("produtos.dat", "rb"); // Carrega o arquivo de produtos como leitura binária
+	int i = 0;
+	
+	if(!arquivo) {
+		erro_arquivo();
+		return;
+	}
+	
+	while(fread(&produtos[i], sizeof(PRODUTO), 1, arquivo)) { // Enquanto houver linhas no arquivo, insere no array de produtos
+		i++;
+	}
+}
+
+void print_produtos(PRODUTO produtos[MAX_PRODUTOS]) { // Exibe na tela os produtos carregados no array de produtos
+	load_produtos(produtos);
+
+	system("cls");
+	printf("Lista de produtos cadastrados: \n\n");
+
+	int i = 0;
+	while(produtos[i].id != NULL) {
+		printf("%d: %s - %d unidades\n", produtos[i].id, produtos[i].nome, produtos[i].estoque);
+
+		i++;
+	}
+	
+	getch();
+}
+
 int main (void) {
 	setlocale(LC_ALL, "");
 	
 	int escolha_menu;
 	bool isValid = true, repeat = true;
 	
-	PRODUTO produtos[100];
-	PEDIDO pedidos[200];
-	PRODUTO_KIT produtos_kit[1000];
-	PRODUTO_PEDIDO produtos_pedido[1000];
-	ORDEM_PRODUCAO ordens_producao[100];
-	PRODUTO_ORDEM_PRODUCAO produtos_op[1000];
-	
+	PRODUTO produtos[MAX_PRODUTOS] = {0};
+	PEDIDO pedidos[200] = {0};
+	PRODUTO_PEDIDO produtos_pedido[1000] = {0};
+	ORDEM_PRODUCAO ordens_producao[100] = {0};
+	PRODUTO_ORDEM_PRODUCAO produtos_op[1000] = {0};
 	
 	// Interação com menus
 	do {
@@ -174,10 +313,10 @@ int main (void) {
 					
 					switch(menu_produtos()) {
 						case 1:
-							//lista
+							print_produtos(produtos);
 							break;
 						case 2:
-							//novo
+							novo_produto();
 							break;
 						case 3: 
 							//atualiza
@@ -207,7 +346,7 @@ int main (void) {
 					
 					switch(menu_estoque()) {
 						case 1:
-							//lista
+							print_produtos(produtos);
 							break;
 						case 2:
 							//ajusta estoque produto
